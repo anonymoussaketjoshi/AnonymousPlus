f1517ed4a8e7014cc6e03f22fade3083b7cedee1
anonymoussaketjoshi <anonymoussaketjoshi@gmail.com>
2017-01-23 16:48:49 +0530

    login button added

--- app/src/main/java/com/saket/attendanceplus/MainActivity.java
+++ app/src/main/java/com/saket/attendanceplus/MainActivity.java
@@ -3,6 +3,7 @@ package com.saket.attendanceplus;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    void nextPage(View view){
         Intent loginpage = new Intent(this,main_login.class);
         startActivity(loginpage);
-
     }
 }
