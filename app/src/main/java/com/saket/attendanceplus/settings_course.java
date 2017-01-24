b2b50b2a1cf219814fc77303cdd33bb6112c45ea
att-akash <sky050897@gmail.com>
2017-01-24 16:10:33 +0530

    Added folder addition on course creation!

--- app/src/main/java/com/saket/attendanceplus/settings_course.java
+++ app/src/main/java/com/saket/attendanceplus/settings_course.java
@@ -1,13 +1,14 @@
 package com.saket.attendanceplus;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.List;
 
 public class settings_course extends AppCompatActivity {
@@ -28,7 +29,7 @@ public class settings_course extends AppCompatActivity {
         textView.setText(textViewText);
     }
 
-
+    //Creates a folder when clicked on add with given name
     public void add_course(View view)  {
         EditText editText1 = (EditText) findViewById(R.id.course_name);
         String courseName = editText1.getText().toString();
@@ -38,11 +39,16 @@ public class settings_course extends AppCompatActivity {
             dBhelper.addCourse(new Course(courseName,courseLink));
             editText1.setText("");
             editText2.setText("");
+            File f = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES).getAbsolutePath()+"/"+courseName);
+            if(!f.exists()) {
+                f.mkdir();
+            }
         }
         else
             Toast.makeText(this,"Enter valid Name and Link!",Toast.LENGTH_LONG).show();
     }
 
+    // No code added to delete the folder
     public void delete_course(View view)   {
         EditText editText1 = (EditText) findViewById(R.id.course_name);
         String courseName = editText1.getText().toString();
@@ -52,7 +58,7 @@ public class settings_course extends AppCompatActivity {
         if(!courseName.equals("") && !courseLink.equals("")) {
             for(int i = 0;i<courseList.size();++i)  {
                 Course course = courseList.get(i);
-                if(course.getCourseName().equals(courseName) && course.getCourseLink().equals(courseLink))
+                if(course.getCourseName().equals(courseName))
                     courseExists = true;
             }
             if(courseExists) {
